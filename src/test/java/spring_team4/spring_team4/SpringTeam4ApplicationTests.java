4bc2222b08b9c608f755cf798e143aaeff6ffa20
woonghee <chw4124@naver.com>
2025-05-12 16:59:48 +0900

    Initial commit

---
+++ src/test/java/spring_team4/spring_team4/SpringTeam4ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_team4.spring_team4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTeam4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
