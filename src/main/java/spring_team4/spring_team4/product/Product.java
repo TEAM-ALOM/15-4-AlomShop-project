75b7b9fb92972e32f35735327525812ff85bc244
woonghee <chw4124@naver.com>
2025-05-13 02:29:41 +0900

    우선 Product 업로드 합니다

---
+++ src/main/java/spring_team4/spring_team4/product/Product.java
@@ -0,0 +1,17 @@
+package spring_team4.spring_team4.product;
+
+import lombok.Getter;
+
+import lombok.Setter;
+
+import jakarta.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Product {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    private Long categoryId;
+    private String productName;
+}
